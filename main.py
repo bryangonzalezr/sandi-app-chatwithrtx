ead5f15711050d8e11dc3ed33639eb8ff95786fd
nrojel <nicole.rojelg@gmail.com>
2024-06-24 19:32:25 -0400

    FIX cors

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ from typing import Union
 from fastapi.responses import StreamingResponse
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import json
 import rtx_api as rtx_api
 
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/chatwithrtx")
 def recibir_respuesta(pregunta: str):
