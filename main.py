3a60fda5cd882db42da63f7006eece5b649c4fdb
NicoPazT <102393798+NicoPazT@users.noreply.github.com>
2024-06-25 23:29:43 -0400

    Respuesta de recetas y consultas

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from typing import Union
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, Response
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import json
 import rtx_api as rtx_api
+from models.userData import UserData
+from models.types import Types
+import httpx
 
 
 app = FastAPI()
@@ -19,8 +22,22 @@ app.add_middleware(
 
 @app.post("/chatwithrtx")
 def recibir_respuesta(pregunta: str):
+    
     def event_generator():
+
         response = rtx_api.send_message(pregunta)
-        yield json.dumps(response)
+        response = dict(response)
+
+        if response['type'] == "General Query":
+            yield json.dumps({"response": response['response']})
+
+        elif response['type'] == 'Recipe':
+            data = json.dumps(response)
+            r = httpx.post("http://localhost:8080/receta/api/", json=response)
+            r.raise_for_status()
+            yield json.dumps(r.json())
+        
+        else:
+            yield json.dumps({"error": "Invalid query type"})
 
     return StreamingResponse(event_generator(), media_type="text/event-stream")
